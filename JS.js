6006ee59f0a9a3a77eba7b3f78c838f2e4d9bebc
= <=>
2022-07-31 19:20:44 +0530

    done

--- JS.js
+++ JS.js
@@ -1,12 +1,12 @@
-let menu = document.querySelector("#menu-icon");
-let navbar= document.querySelector('.navbar');
-menu.addEventListner("click",()=>{
+ const menu = document.querySelector('#menu-icon');
+ const navbar= document.querySelector('.navbar');
+menu.addEventListener('click',()=>{
   menu.classList.toggle('bx-x');
   navbar.classList.toggle('active');
 })
 
-// window.onscroll = ()=>{
-//   menu.classList.toggle('bx-x');
-//   navbar.classList.toggle('active');
-//
-// }
+
+navbar.addEventListener('scroll',()=>{
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
+})
